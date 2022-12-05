199c29249009e94751b40ea8b455ddf71728e6b0
Michał Bojara <misko20023@wp.pl>
2022-12-05 20:40:29 +0100

    initial commit

---
+++ WPFConverter/Models/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFConverter.Models
+{
+    internal class Converter
+    {
+        int _base { get; set; }
+        string _inputvalue { get; set; }
+        string _outputvalue { get; set; }
+
+    }
+}
---
+++ WPFConverter/ViewModels/ShellViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFConverter.ViewModels
+{
+    public class ShellViewModel:Screen
+    {
+		private string _inputVal="lol zmieniłem wartość";
+
+		public string InputVal
+		{
+			get { return _inputVal; }
+			set { _inputVal = value; }
+		}
+
+		private string _outputVal="0";
+
+		public string OutputVal
+		{
+			get { return _outputVal; }
+			set { _outputVal = value; }
+		}
+
+
+	}
+
+}
